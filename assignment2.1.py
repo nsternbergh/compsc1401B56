de148116c6e64f03bcb501931fa93132a87bab5a
nsternbergh <111912713+nsternbergh@users.noreply.github.com>
2022-09-06 14:23:42 -0700

    Add files via upload

---
+++ assignment2.1.py
@@ -0,0 +1,8 @@
+male_students = int(input('Enter male students'))
+female_students = int(input('Enter female students'))
+total_students = int(input('Enter total students'))
+
+
+print("Female students {}". format(female_students))
+print("Male students {}". format(male_students))
+print("Total students {}". format(total_students))
